12f83b5a39ebad1537c38d93278fc337a91c947f
Brian Lynnerup Pedersen <brian.lynnerup.pedersen@gmail.com>
2024-11-16 22:48:51 +0100

    Basic chat bot setup

---
+++ The News Agent/The News Agent/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+var builder = Kernel.CreateBuilder();
+
+// Services
+builder.AddAzureOpenAIChatCompletion(
+    "gpt-4-news",
+    "[endpoint]",
+    "[apiKey]");
+
+// Plugins
+
+var kernel = builder.Build();
+
+var chatService = kernel.GetRequiredService<IChatCompletionService>();
+var chatHistory = new ChatHistory();
+
+while (true)
+{
+    Console.Write("Prompt:");
+    chatHistory.AddUserMessage(Console.ReadLine());
+
+    var completion = chatService.GetStreamingChatMessageContentsAsync(chatHistory, kernel: kernel);
+
+    var response = "";
+    await foreach (var content in completion)
+    {
+        Console.Write(content.Content);
+        response += content.Content;
+    }
+    
+    chatHistory.AddAssistantMessage(response);
+    Console.WriteLine(response);
+}
+
